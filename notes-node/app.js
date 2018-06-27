044e7614006c8bbf57ee35433a269feb854f0e81
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-06-27 20:07:17 +0530

    'Require'ing fs and os modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,5 +1,11 @@
 console.log('Starting app');
 
 const fs = require('fs');
+const os = require('os');
+var user = os.userInfo();
 
-fs.appendFileSync('Greetings.txt', 'Hello World!!');
+//fs.appendFileSync('Greetings.txt', 'Hello '+user.username);
+
+fs.appendFileSync('Greetings.txt', `Hello ${user.username}`);
+
+
