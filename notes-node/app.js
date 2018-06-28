ae9e0b7665fa689aa57f730214f2d7f7678b7cfd
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-06-28 05:33:11 +0530

    Added argument checks

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,18 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command:', command);
+
+if(command == 'add'){
+    console.log('Adding new note');
+}else if(command == 'list'){
+    console.log('List all notes');
+}else if(command == 'read'){
+    console.log('Reading node');
+}else if(command == 'remove'){
+    console.log('Deleting note');
+}else{
+    console.log('Operation not found');
+}
