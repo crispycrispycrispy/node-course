c16bfb852d531b02faa42138b0d85e59e536937b
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-07-19 18:29:17 +0530

    testing debugging functions, arrow functions

---
+++ notes-node/playground/arrow-functuin.js
@@ -0,0 +1,5 @@
+var square = (x) => {
+    var result = x*x;
+    return result;
+}
+console.log(square(10));
---
+++ notes-node/playground/debugging.js
@@ -0,0 +1,8 @@
+var person = {
+    name : 'Andrew'
+};
+person.age = 20;
+debugger;
+person.name = 'Mike';
+
+console.log(person);
