a2453111deb10d41b455b842004b2a5d9518d689
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-07-27 05:23:35 +0530

    Create new weather-app folder; require yargs,request; playground folder

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,7 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json : true
 }, (error, response, body) => {
-    console.log(body);
+    // console.log(JSON.stringify(body, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat} Longitude: ${body.results[0].geometry.location.lon}`);
 });
