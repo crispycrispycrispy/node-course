ce9160e1472517f7d39d5925ce2e2a005e259c7d
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-07-28 15:31:41 +0530

    Abstraction of app.js to geocode.js with callbacks

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
-var request = require('request');
-var yargs = require('yargs');
+var geocode = require('./geocode/geocode.js');
 
+var yargs = require('yargs');
 var argv = yargs
 .options({
     a: {
@@ -15,21 +15,8 @@ var argv = yargs
 .argv;
 console.log(argv);
 
-var address = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
-    json : true
-}, (error, response, body) => {
-    if(error){
-        console.log('Unable to connect to google servers');
-    }
-    else if(body.status === 'ZERO_RESULTS'){
-        console.log('unable to find address');
-    }
-    else if(body.status === 'OK'){
-        // console.log(JSON.stringify(body, undefined, 2));
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat} Longitude: ${body.results[0].geometry.location.lng}`);
-    }
+geocode.getGeocodeAddress(argv.address, (errorMsg, results) => {
+    if(errorMsg) console.log(errorMsg)
+    else console.log(JSON.stringify(results, undefined, 2))
 });
+
---
+++ weather-app/geocode/geocode.js
@@ -0,0 +1,27 @@
+var request = require('request');
+
+var getGeocodeAddress = (address,  callback) => {
+    var encodedaddress = encodeURIComponent(address);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedaddress}`,
+        json : true
+    }, (error, response, body) => {
+        if(error){
+            callback('Unable to connect to google servers');
+        }
+        else if(body.status === 'ZERO_RESULTS'){
+            callback('unable to find address');
+        }
+        else if(body.status === 'OK'){
+            callback(undefined, {
+                address : body.results[0].formatted_address,
+                latitude : body.results[0].geometry.location.lat,
+                longitude : body.results[0].geometry.location.lng
+            });
+        }
+    });
+};
+
+module.exports = {
+    getGeocodeAddress
+}
