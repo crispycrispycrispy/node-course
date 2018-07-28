245798a89527f322d0da2477182d50436f6b9c8a
Gavin Dsilva <gavinchrisdsilva@gmail.com>
2018-07-28 15:01:06 +0530

    Added address through cmd through yargs

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,27 @@
 var request = require('request');
+var yargs = require('yargs');
+
+var argv = yargs
+.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+    }
+})
+.help()
+.alias('help','h')
+.argv;
+console.log(argv);
+
+var address = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json : true
 }, (error, response, body) => {
     // console.log(JSON.stringify(body, undefined, 2));
     console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat} Longitude: ${body.results[0].geometry.location.lon}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat} Longitude: ${body.results[0].geometry.location.lng}`);
 });
