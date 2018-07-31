c27f0c1e12a5b63caa67ef04be10b86d955d8973
Gavin <gavinchrisdsilva@gmail.com>
2018-07-31 23:18:47 +0530

    Promises in geocode api

--- weather-app/playground/promise-2.js
+++ weather-app/playground/promise-2.js
@@ -1,10 +1,10 @@
 var request = require('request');
-
+var key = 'AIzaSyB--MniXKdzk5q6lBWCe8xNEH_5jx6qmi0';
 var geocodeAddress = (address) => {
     return new Promise( (resolve, reject) => {
         var encodedaddress = encodeURIComponent(address);
         request({
-            url: `https://maapsgoogleapis.com/maps/api/geocode/json?address=${encodedaddress}`,
+            url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedaddress}&key=${key}`,
             json : true
         }, (error, response, body) => {
             if(error){
@@ -30,25 +30,3 @@ geocodeAddress(400074).then(
     },
     (error) => { console.log(error); }
 )
-
-// var getgeocodeAddress = (address,  callback) => {
-//     var encodedaddress = encodeURIComponent(address);
-//     request({
-//         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedaddress}`,
-//         json : true
-//     }, (error, response, body) => {
-//         if(error){
-//             callback('Unable to connect to google servers');
-//         }
-//         else if(body.status === 'ZERO_RESULTS'){
-//             callback('unable to find address');
-//         }
-//         else if(body.status === 'OK'){
-//             callback(undefined, {
-//                 address : body.results[0].formatted_address,
-//                 lat : body.results[0].geometry.location.lat,
-//                 lon : body.results[0].geometry.location.lng
-//             });
-//         }
-//     });
-// };
